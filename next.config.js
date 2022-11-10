fc894e991876d318bf2ffb20fac0076936296108
jacobean.eth <jacobsmall@jacobs-MacBook-Pro-2.local>
2022-11-10 14:09:26 -0800

    new config file

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-const withPlugins = require('next-compose-plugins');
-const withImages = require('next-images');
-const withSvgr = require('next-svgr');
+
 
 module.exports = withPlugins(
-  [
-    [
-      withImages,
-      {
-        fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'ico', 'webp', 'jp2', 'avif'],
-      },
-    ],
-    [withSvgr],
-  ],
   {
     eslint: {
       // Warning: This allows production builds to successfully complete even if
