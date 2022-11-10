9959eed2c4ef78c40a5450a72cd4e1bfcbfab074
jacobean.eth <jacobsmall@jacobs-MacBook-Pro-2.local>
2022-11-10 14:21:33 -0800

    added next image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+const withSvgr = require('next-svgr');
 
 module.exports = withPlugins(
+  [
+    [
+      withImages,
+      {
+        fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'ico', 'webp', 'jp2', 'avif'],
+      },
+    ],
+    [withSvgr],
+  ],
   {
     eslint: {
       // Warning: This allows production builds to successfully complete even if
